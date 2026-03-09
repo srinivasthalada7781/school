473732d37c7d586cf99dff9d1682a0ec17c60ade
srinivasthalada7781 <srinivasthalada7781@gmail.com>
2026-03-09 23:41:26 +0530

    style: Reduce gallery photos down to 8 images

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -7,22 +7,8 @@ const images = [
   { src: "/pictures/event1.png", alt: "School celebration", span: "md:col-span-2" },
   { src: "/pictures/group.png", alt: "Outdoor games", span: "" },
   { src: "/pictures/study2.png", alt: "Story time with teacher", span: "" },
-  // New images
-  { src: "/pictures/event2.png", alt: "Cultural event", span: "" },
-  { src: "/pictures/event4.png", alt: "Stage performance", span: "" },
+  // 2 New Extra Images
   { src: "/pictures/event6.png", alt: "Special celebration", span: "md:col-span-2" },
-  { src: "/pictures/event7.png", alt: "School festival", span: "" },
-  { src: "/pictures/extra c2.png", alt: "Creative playtime", span: "" },
-  { src: "/pictures/extra c3.png", alt: "Hands-on learning", span: "" },
-  { src: "/pictures/extra c4.png", alt: "Interactive activity", span: "md:col-span-2" },
-  { src: "/pictures/faculty.png", alt: "Our dedicated faculty", span: "" },
-  { src: "/pictures/happy1.png", alt: "Joyful moments", span: "" },
-  { src: "/pictures/happy2.png", alt: "Smiling students", span: "" },
-  { src: "/pictures/prize1.png", alt: "Award distribution", span: "md:col-span-2" },
-  { src: "/pictures/prize2.png", alt: "Student achievement", span: "" },
-  { src: "/pictures/study1.png", alt: "Focused reading", span: "" },
-  { src: "/pictures/group photo 1.png", alt: "Class group photo", span: "" },
-  { src: "/pictures/group photo 2.png", alt: "School group photo", span: "md:col-span-2 md:row-span-2" },
   { src: "/pictures/group photo.png", alt: "Students together", span: "" },
 ];
 
